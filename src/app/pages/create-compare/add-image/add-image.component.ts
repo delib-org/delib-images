66bf37cd52cb43335771cb1a44bf3ba0bb9da0c2
Tal <tal.yaron@gmail.com>
2021-12-31 15:01:35 +0200

    created add image component

--- src/app/pages/create-compare/add-image/add-image.component.ts
+++ src/app/pages/create-compare/add-image/add-image.component.ts
@@ -1,18 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,NgZone  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FileUploader, FileUploaderOptions, ParsedResponseHeaders } from 'ng2-file-upload';
+import { Cloudinary } from '@cloudinary/angular-5.x';
 
 @Component({
   selector: 'app-add-image',
   templateUrl: './add-image.component.html',
   styleUrls: ['./add-image.component.scss']
 })
-export class AddImageComponent implements OnInit {
+export class AddImageComponent  implements OnInit {
 
-  
+  @Input() responses: Array<any>;
 
-  constructor() {}
-    
+  private hasBaseDropZoneOver: boolean = false;
+  private uploader: FileUploader | undefined;
+  private title: string;
 
-  ngOnInit(): void {
+  constructor(
+    private cloudinary: Cloudinary,
+    private zone: NgZone,
+    private http: HttpClient
+  ) {
+    this.responses = [];
+    this.title = '';
   }
 
+  ngOnInit(): void {
+    // Create the file uploader, wire it to upload to your account
+    const uploaderOptions: FileUploaderOptions = {
+      url: `https://api.cloudinary.com/v1_1/${this.cloudinary.config().cloud_name}/upload`,
+      // Upload files automatically upon addition to upload queue
+      autoUpload: true,
+      // Use xhrTransport in favor of iframeTransport
+      isHTML5: true,
+      // Calculate progress independently for each uploaded file
+      removeAfterUpload: true,
+      // XHR request headers
+      headers: [
+        {
+          name: 'X-Requested-With',
+          value: 'XMLHttpRequest'
+        }
+      ]
+    };
+
+    this.uploader = new FileUploader(uploaderOptions);
+
+    this.uploader.onBuildItemForm = (fileItem: any, form: FormData): any => {
+      // Add Cloudinary unsigned upload preset to the upload form
+      form.append('upload_preset', this.cloudinary.config().upload_preset);
+
+      // Add file to upload
+      form.append('file', fileItem);
+
+      // Use default "withCredentials" value for CORS requests
+      fileItem.withCredentials = false;
+      return { fileItem, form };
+    };
+  }
+
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
 }
